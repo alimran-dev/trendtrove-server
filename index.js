2bcdef6f0cdac3bc0212273e4346b7f7ffa5de6c
alimran-dev <alimran5air@gmail.com>
2023-10-20 21:23:49 +0600

    product update api added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -13,9 +13,6 @@ app.get("/", (req, res) => {
   res.send("Brand shop server is running...");
 });
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tklaef2.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,41 +21,78 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-      // await client.connect();
-      const database = client.db('brandShopDB');
-      const brandCollection = database.collection('brands');
-      const productsCollection = database.collection('products');
+    // await client.connect();
+    const database = client.db("brandShopDB");
+    const brandCollection = database.collection("brands");
+    const productsCollection = database.collection("products");
+    const cartCollection = database.collection("cart");
 
-      app.get('/brands',async (req, res) => {
-          const cursor = brandCollection.find();
-          const result = await cursor.toArray();
-          res.send(result);
-      })
-      app.get('/products/:brand', async (req, res) => {
-        const brand = req.params.brand;
-          const filter = { brand: brand };
-          const cursor = productsCollection.find(filter);
-          const result = await cursor.toArray();
-          res.send(result);
-      })
+    app.get("/brands", async (req, res) => {
+      const cursor = brandCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/products/:brand", async (req, res) => {
+      const brand = req.params.brand;
+      const filter = { brand: brand };
+      const cursor = productsCollection.find(filter);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+    app.get("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const cursor = productsCollection.find(filter);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/cart", async (req, res) => {
+      const product = req.body;
+      const result = await cartCollection.insertOne(product);
+      res.send(result);
+    });
+    app.put("/product/:id", async (req, res) => {
+      const id = req.params.id;
+      const product = req.body;
+      const filter = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updatedProduct = {
+        $set: {
+          name: product.name,
+          brand: product.brand,
+          price: product.price * 1,
+          type: product.type,
+          rating: product.rating * 1,
+          photo: product.photo,
+          description: product.description,
+        },
+      };
+      const result = await productsCollection.updateOne(
+        filter,
+        updatedProduct,
+        options
+      );
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
 run().catch(console.dir);
-
-
 
 app.listen(port, () => {
   console.log(`Brand shop server is running on port: ${port}`);
